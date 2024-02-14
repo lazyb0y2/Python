7a1be02712841dacdb1d7847a560b9e0cfa52547
lazyb0y2 <mjones60@bostonk12.org>
2024-02-14 10:51:05 -0500

    some of the basics

---
+++ main.py
@@ -0,0 +1,9 @@
+# outputs words on a screen
+print("hello")
+# this is a variable it contains the value of the thing inside of it
+name = "bro"
+# don't add " to a variable because it will become a string the outputs the variable name not what's contained
+print("name")
+print(name)
+# this shows the variables value
+print(type(name))
