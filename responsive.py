72d3d1600191b15974bada6b2b697bc9125b2b28
lazyb0y2 <mjones60@bostonk12.org>
2024-02-14 13:59:38 -0500

    improved the dialog

--- responsive.py
+++ responsive.py
@@ -1,4 +1,4 @@
 first_name = input("whats your first name:")
 last_name = input("whats your last name:")
 full_name = first_name + " " + last_name
-print(f"so your name is {full_name}")
+print(f"so your full name is {full_name}")
