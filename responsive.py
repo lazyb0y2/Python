afcabc06444557e1ab8882eaf8cf8fb12dadd519
lazyb0y2 <mjones60@bostonk12.org>
2024-02-14 14:23:41 -0500

    added if statement

--- responsive.py
+++ responsive.py
@@ -2,7 +2,22 @@ first_name = input("whats your first name:")
 last_name = input("whats your last name:")
 full_name = first_name + " " + last_name
 print(f"so your full name is {full_name}")
+
 num = input("choose an number:")
 num2 = input("choose an number:")
 solution = int(num) + int(num2)
 print(f"so if you add {num} and {num2} you'll get {solution}")
+
+response = input("which spongebob character is your favorite spongebob, patrick, squidward, or sandy")
+
+
+if response == "spongebob":
+  result = "your based"
+elif response == "patrick":
+  result = "you like dumb people"
+elif response == "squidward":
+  result = "your an adult"
+elif response = "sandy":
+  result = "your a simp"
+
+print(result)
