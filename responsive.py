d81c1ad07e5724a639d14fead01182407a0b35a7
lazyb0y2 <mjones60@bostonk12.org>
2024-02-14 14:07:15 -0500

    added a whole math equation

--- responsive.py
+++ responsive.py
@@ -2,3 +2,7 @@ first_name = input("whats your first name:")
 last_name = input("whats your last name:")
 full_name = first_name + " " + last_name
 print(f"so your full name is {full_name}")
+num = input("choose an number:")
+num2 = input("choose an number:")
+solution = int(num) + int(num2)
+print(f"so if you add {num} and {num2} you'll get {solution}")
