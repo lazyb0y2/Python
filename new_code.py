afd752e1ccd5a6b37890d5fbfe2cecd31004f3b9
lazyb0y2 <mjones60@bostonk12.org>
2024-02-15 13:41:13 -0500

    I reversed the gravity on variable name

--- new_code.py
+++ new_code.py
@@ -7,6 +7,8 @@ name = "Bro Code"
 first_name = name[0:3]  # or [:3]
 last_name = name[4:8]  # or [4:]
 funky_name = name[0:8:2]
+reversed_name = name[::-1]
 print(first_name)
 print(last_name)
 print(funky_name)
+print(reversed_name)
