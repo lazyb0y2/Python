1ecc7e31066c301594140d3b9f01f65c88084cab
lazyb0y2 <mjones60@bostonk12.org>
2024-02-15 13:37:58 -0500

    act three steps

--- new_code.py
+++ new_code.py
@@ -4,7 +4,9 @@
 
 name = "Bro Code"
 
-first_name = name[0:3]
-last_name = name[4:9]
+first_name = name[0:3]  # or [:3]
+last_name = name[4:8]  # or [4:]
+funky_name = name[0:8:2]
 print(first_name)
 print(last_name)
+print(funky_name)
