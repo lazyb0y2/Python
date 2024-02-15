2923a8920413081d47344bb79305da69fa93428c
lazyb0y2 <mjones60@bostonk12.org>
2024-02-15 07:45:45 -0500

    changed formatting

--- responsive.py
+++ responsive.py
@@ -10,14 +10,14 @@ print(f"so if you add {num} and {num2} you'll get {solution}")
 
 response = input("which spongebob character is your favorite spongebob, patrick, squidward, or sandy: ")
 
-
 if response == "spongebob":
-  result = "your based"
+    result = "your based"
 elif response == "patrick":
-  result = "you like dumb people"
+    result = "you like dumb people"
 elif response == "squidward":
-  result = "your an adult"
+    result = "your an adult"
 elif response == "sandy":
-  result = "your a simp"
-else: "okay keep your secrets"
+    result = "your a simp"
+else:
+    "okay keep your secrets"
 print(result)
