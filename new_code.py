f32491252272f933edafdd919911d3644175efa9
lazyb0y2 <mjones60@bostonk12.org>
2024-02-16 08:29:45 -0500

    slice and website variable

--- new_code.py
+++ new_code.py
@@ -3,7 +3,7 @@
 #           [start:stop:step]
 
 name = "Bro Code"
-
+# I'm going to be indexing in this section
 first_name = name[0:3]  # or [:3]
 last_name = name[4:8]  # or [4:]
 funky_name = name[0:8:2]  # or [::2]
@@ -12,3 +12,14 @@ print(first_name)
 print(last_name)
 print(funky_name)
 print(reversed_name)
+# ----------------------------------------------------------------------------------------------------------------------
+# In this section im going to be slicing
+
+website1 = "https://google.com"
+website2 = "https://wikipedia.com"
+
+
+slice = slice(8, -4)
+
+print(website1[slice])
+print(website2[slice])
