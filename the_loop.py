4fff1fa12723485d6f81701c88834b271920140c
lazyb0y2 <mjones60@bostonk12.org>
2024-03-04 12:35:44 -0500

    comments out range code

--- the_loop.py
+++ the_loop.py
@@ -20,8 +20,8 @@
 #              while loop = unlimited
 #              for loop = limited
 
-for i in range(10):
-    print(i + 1)
+# for i in range(10):
+   # print(i + 1)
 
 # for i in range (50,100+1,2):
 # print(i)
