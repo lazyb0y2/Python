235cf09938cf868fccb4cfb0166458fc0a03e56a
lazyb0y2 <mjones60@bostonk12.org>
2024-03-04 10:50:15 -0500

    improved the loop.

--- the_loop.py
+++ the_loop.py
@@ -1,6 +1,9 @@
 # while loop = a statement that will execute its block of code
 #              as long as it's condition remains true
 
-while 1 == 1:
-    print("Help! I'm stuck in a loop!")
+name = ""
+while len(name) == 0:
+    name = input("Enter your name: ")
+
+print(name)
 # ----------------------------------------------------------------------------------------------------------------------
