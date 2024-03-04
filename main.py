503b832b85b09d7ab08d38c6b39574f33c6e7156
lazyb0y2 <mjones60@bostonk12.org>
2024-03-04 10:45:14 -0500

    change the comment a bit and some changes to the loop py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 print("hello")
 # ----------------------------------------------------------------------------------------------------------------------
-# this is a variable it contains the value of the thing inside of it
+# this is a variable it contains the value of the thing inside it
 name = "bro"
 # don't add " to a variable because it will become a string the outputs the variable name not what's contained
 print("name")
