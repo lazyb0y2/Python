174f3e71cf248fe13766fb46b049d2ef245d0421
lazyb0y2 <mjones60@bostonk12.org>
2024-03-04 11:11:41 -0500

    comment out

--- the_loop.py
+++ the_loop.py
@@ -1,9 +1,15 @@
 # while loop = a statement that will execute its block of code
 #              as long as it's condition remains true
 
-name = ""
-while len(name) == 0:
+# name = ""
+# while len(name) == 0:
+#   name = input("Enter your name: ")
+
+# print("Hello" + " " + name)
+# ----------------------------------------------------------------------------------------------------------------------
+name = None
+
+while not name:
     name = input("Enter your name: ")
 
-print("Hello" + " " + name)
-# ----------------------------------------------------------------------------------------------------------------------
+    print("Hello "+name)
