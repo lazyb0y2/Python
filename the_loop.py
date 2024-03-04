08c939cfba348b07b16db9db0cf8c14cd544792b
lazyb0y2 <mjones60@bostonk12.org>
2024-03-04 11:58:19 -0500

    comments about loops

--- the_loop.py
+++ the_loop.py
@@ -13,3 +13,18 @@ while not name:
     name = input("Enter your name: ")
 
     print("Hello "+name)
+# ----------------------------------------------------------------------------------------------------------------------
+# for loop =   a statement that will execute its block of code
+#              a limited amount of times
+#
+#              while loop = unlimited
+#              for loop = limited
+
+# for i in range(10):
+     # print(i+1)
+
+# for i in range (50,100+1,2):
+     # print(i)
+
+# for i in "Bro Code":
+     # print(i)
