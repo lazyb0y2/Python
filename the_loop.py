a16c090d6c9768b2cb1528c2ecbf2842931a82f3
lazyb0y2 <mjones60@bostonk12.org>
2024-03-04 09:56:12 -0500

    looped message with an explamation

--- the_loop.py
+++ the_loop.py
@@ -1,2 +1,4 @@
+# causes the text in name to constantly be printed in a loop
 while 1 == 1:
     name = print("Help! I'm stuck in a loop!")
+# ----------------------------------------------------------------------------------------------------------------------
