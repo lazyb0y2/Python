6dc9ae519df210ce456ae5f9d0dca5a72906e1fa
lazyb0y2 <mjones60@bostonk12.org>
2024-03-18 11:01:21 -0400

    starting dictionary

--- main.py
+++ main.py
@@ -164,4 +164,7 @@ dishes = {"bowl", "plate", "cup","knife"}
 
 #for x in dinner_table:
     #print(x)
+# ----------------------------------------------------------------------------------------------------------------------
+# dictionary =  A changeable, unordered collection of a unique key:value pairs
+#               Fast because they use hashing, allow us to access a value quickly
 
