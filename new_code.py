9b6721643e8a05e315436899dbd824b9c0866871
lazyb0y2 <mjones60@bostonk12.org>
2024-03-19 12:37:50 -0400

    A code implant

--- new_code.py
+++ new_code.py
@@ -46,3 +46,6 @@ slice = slice(8, -4)
 
 #print(x)
 # ----------------------------------------------------------------------------------------------------------------------
+#   Keyword argument = argument preceded by an identifier when we pass them to a function
+#                      The order of the argument doesn't matter, unlike positional arguments
+#                      Python knows the names of the arguments that our function receives
