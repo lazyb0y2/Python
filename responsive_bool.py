f382ca1eb240f0dfcda11ac1822b6e8edf477fec
lazyb0y2 <mjones60@bostonk12.org>
2024-03-19 12:29:54 -0400

    The end of return statement

--- responsive_bool.py
+++ responsive_bool.py
@@ -86,10 +86,12 @@
 #   return statement = Functions send python values/objects back to the caller.
 #                      These values/objects are known as the function's return value
 
-def multiply(number1, number2):
-    result = number1 * number2
-    return result
+#def multiply(number1, number2):
+#    result = number1 * number2
+#    return number1 * number2
 
-x = multiply(6, 8)
+#x = multiply(6, 8)
 
-print(x)
+#print(x)
+# ----------------------------------------------------------------------------------------------------------------------
+#   Keyword argument = argument
