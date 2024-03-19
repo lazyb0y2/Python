f10bff1f9499b7b182ed507555943c59dcadea7a
lazyb0y2 <mjones60@bostonk12.org>
2024-03-19 10:26:30 -0400

    accidental format and working on def

--- responsive_bool.py
+++ responsive_bool.py
@@ -1,78 +1,84 @@
 # input gives you prompt to answer where your response will be stored in the variable
-#first_name = input("whats your first name:")
-#last_name = input("whats your last name:")
-#full_name = first_name + " " + last_name
-#print(f"so your full name is {full_name}")
+# first_name = input("whats your first name:")
+# last_name = input("whats your last name:")
+# full_name = first_name + " " + last_name
+# print(f"so your full name is {full_name}")
 # ----------------------------------------------------------------------------------------------------------------------
 # ask for two numbers and adds them together
-#num = input("choose an number:")
-#num2 = input("choose another number:")
-#solution = int(num) + int(num2)
-#print(f"so if you add {num} and {num2} you'll get {solution}")
+# num = input("choose an number:")
+# num2 = input("choose another number:")
+# solution = int(num) + int(num2)
+# print(f"so if you add {num} and {num2} you'll get {solution}")
 # ----------------------------------------------------------------------------------------------------------------------
 # giving the user a response to there favorite character from spongebob
-#response = input("which spongebob character is your favorite spongebob, patrick, squidward, krabs, plankton, or sandy: ")
+# response = input("which spongebob character is your favorite spongebob, patrick, squidward, krabs, plankton, or sandy: ")
 
-#if response == "spongebob":
+# if response == "spongebob":
 #    result = "your based"
-#elif response == "patrick":
+# elif response == "patrick":
 #    result = "you like dumb people"
-#elif response == "squidward":
+# elif response == "squidward":
 #    result = "your an adult"
-#elif response == "sandy":
+# elif response == "sandy":
 #    result = "your a simp"
-#elif response == "krabs":
+# elif response == "krabs":
 #    result = "your greedy and stingy"
-#elif response == "mr.krabs":
+# elif response == "mr.krabs":
 #    result = "your greedy and stingy"
-#elif response == "plankton":
+# elif response == "plankton":
 #    result = "you don't exist"
-#elif response == "gary":
+# elif response == "gary":
 #    result = "you are a god"
-#else:
+# else:
 #    result = "okay keep your secrets"
-#print(result)
+# print(result)
 # ----------------------------------------------------------------------------------------------------------------------
 # asking user how old they are and giving them a statement about there age
-#age = int(input("How old are you currently?: "))
+# age = int(input("How old are you currently?: "))
 
-#if age >= 100:
+# if age >= 100:
 #    print("How? whats your secret")
-#elif age == 100:
+# elif age == 100:
 #    print("Sorry for being so blunt but your old!")
-#elif age >= 75:
+# elif age >= 75:
 #    print("So your a senior")
-#elif age >= 50:
+# elif age >= 50:
 #    print("So your middle aged")
-#elif age >= 18:
+# elif age >= 18:
 #    print("So your an adult")
-#elif age >= 13:
+# elif age >= 13:
 #    print("So your a teenager")
-#else:
+# else:
 #    print("Your a child")
 # ----------------------------------------------------------------------------------------------------------------------
 # Logical operated (and,or,not) = used to check if two or more conditional statements is True
 
-#temp = int(input("Whats the temperature outside?: "))
+# temp = int(input("Whats the temperature outside?: "))
 # and means that both statements need be correct to be true
-#if (temp >= 0 and temp <= 30):
+# if (temp >= 0 and temp <= 30):
 #    print("The temperature is good today!")
 #    print("go outside!")
 # or means one or both statements need to be correct for it to be true
-#elif (temp < 0 or temp > 30):
+# elif (temp < 0 or temp > 30):
 #    print("The temperature is bad today!")
 #    print("Stay inside!")
 
 # these if not mean that if statements wrong It's correct
-#if not (temp >= 0 and temp <= 30):
+# if not (temp >= 0 and temp <= 30):
 #    print("The temperature is bad today!")
 #    print("Stay inside!")
 
-#elif not (temp < 0 or temp > 30):
+# elif not (temp < 0 or temp > 30):
 #    print("The temperature is good today!")
 #    print("go outside!")
 # ----------------------------------------------------------------------------------------------------------------------
 #   function = a block of code which is executed only when it is called
 
-def repeat():
-    print("I will not repeat my code")
+# I guess it allows you to use a trigger frase that activates its function if I input the name after def
+def hello(start_name, end_name, life):
+    print("hello!"+start_name+" "+end_name)
+    print("You are "+str(life)+" years old") # it doesn't like ints and floats
+    print("have a nice day!")
+
+
+hello("Bro", "Code", 21)
