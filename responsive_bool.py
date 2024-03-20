6b36f14ab0ae42290abe6d0deb3454fca403d057
lazyb0y2 <mjones60@bostonk12.org>
2024-03-20 08:22:53 -0400

    modification #2

--- responsive_bool.py
+++ responsive_bool.py
@@ -1,54 +1,54 @@
 # input gives you prompt to answer where your response will be stored in the variable
-# first_name = input("whats your first name:")
-# last_name = input("whats your last name:")
-# full_name = first_name + " " + last_name
-# print(f"so your full name is {full_name}")
+#first_name = input("whats your first name:")
+#last_name = input("whats your last name:")
+#full_name = first_name + " " + last_name
+#print(f"so your full name is {full_name}")
 # ----------------------------------------------------------------------------------------------------------------------
 # ask for two numbers and adds them together
-# num = input("choose an number:")
-# num2 = input("choose another number:")
-# solution = int(num) + int(num2)
-# print(f"so if you add {num} and {num2} you'll get {solution}")
+#num = input("choose an number:")
+#num2 = input("choose another number:")
+#solution = int(num) + int(num2)
+#print(f"so if you add {num} and {num2} you'll get {solution}")
 # ----------------------------------------------------------------------------------------------------------------------
 # giving the user a response to there favorite character from spongebob
-# response = input("which spongebob character is your favorite spongebob, patrick, squidward, krabs, plankton, or sandy: ")
+#response = input("which spongebob character is your favorite spongebob, patrick, squidward, krabs, plankton, or sandy: ")
 
-# if response == "spongebob":
+#if response == "spongebob":
 #    result = "your based"
-# elif response == "patrick":
+#elif response == "patrick":
 #    result = "you like dumb people"
-# elif response == "squidward":
+#elif response == "squidward":
 #    result = "your an adult"
-# elif response == "sandy":
+#elif response == "sandy":
 #    result = "your a simp"
-# elif response == "krabs":
+#elif response == "krabs":
 #    result = "your greedy and stingy"
-# elif response == "mr.krabs":
+#elif response == "mr.krabs":
 #    result = "your greedy and stingy"
-# elif response == "plankton":
+#elif response == "plankton":
 #    result = "you don't exist"
-# elif response == "gary":
+#elif response == "gary":
 #    result = "you are a god"
-# else:
+#else:
 #    result = "okay keep your secrets"
-# print(result)
+#print(result)
 # ----------------------------------------------------------------------------------------------------------------------
 # asking user how old they are and giving them a statement about there age
-# age = int(input("How old are you currently?: "))
+#age = int(input("How old are you currently?: "))
 
-# if age >= 100:
+#if age >= 100:
 #    print("How? whats your secret")
-# elif age == 100:
+#elif age == 100:
 #    print("Sorry for being so blunt but your old!")
-# elif age >= 75:
+#elif age >= 75:
 #    print("So your a senior")
-# elif age >= 50:
+#elif age >= 50:
 #    print("So your middle aged")
-# elif age >= 18:
+#elif age >= 18:
 #    print("So your an adult")
-# elif age >= 13:
+#elif age >= 13:
 #    print("So your a teenager")
-# else:
+#else:
 #    print("Your a child")
 # ----------------------------------------------------------------------------------------------------------------------
 # Logical operated (and,or,not) = used to check if two or more conditional statements is True
