d6751721c7c9c1c30ecdf4d3aee76e43c27d6aa8
lazyb0y2 <mjones60@bostonk12.org>
2024-03-20 09:44:14 -0400

    commented out some print statements

--- main.py
+++ main.py
@@ -198,7 +198,7 @@ first_section = title[:4].upper() # outputs value 0 to 4 while keeping it upper-
 last_section = title[5:].lower() # outputs value 5 to 9 while keeping it lower-case
 last_character = title[-1] # outputs a single character by counting in reverse / in negatives
 
-print(first_section)
-print(last_section)
-print(last_character)
+#print(first_section)
+#print(last_section)
+#print(last_character)
 # ----------------------------------------------------------------------------------------------------------------------
