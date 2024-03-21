57ef6e8a2b3a00c611d63c17929a43696d3c9b34
lazyb0y2 <mjones60@bostonk12.org>
2024-03-21 12:55:07 -0400

    Working on str.format #3

--- new_code.py
+++ new_code.py
@@ -84,4 +84,5 @@
 animal = "cow"
 item = "moon"
 
-print("The "+animal+" jumped over the "+item)
+#print("The "+animal+" jumped over the "+item)
+print("The {} jumped over the {}".format(animal, item))
