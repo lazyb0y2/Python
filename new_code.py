d67ea32ed960dda37eeca26ec07b3f4b5981334b
lazyb0y2 <mjones60@bostonk12.org>
2024-03-21 12:22:43 -0400

    working on **kwargs #1

--- new_code.py
+++ new_code.py
@@ -80,3 +80,6 @@
 # ----------------------------------------------------------------------------------------------------------------------
 # **kwargs =    parameter that will pack all arguments into the dictionary
 #               useful so that a function can accept a varying number of keywords' arguments
+
+def hello(first, last):
+    print("Hello ", + first +" ",+last)
