11013b2f4bcd1c31fbf59137af37bf3eae1bd5ff
lazyb0y2 <mjones60@bostonk12.org>
2024-03-21 13:35:33 -0400

    Working on str.format #6

--- new_code.py
+++ new_code.py
@@ -85,5 +85,6 @@ animal = "cow"
 item = "moon"
 
 #print("The "+animal+" jumped over the "+item)
-print("The {} jumped over the {}".format(animal, item))
-print("The {0} jumped over the {1}".format(animal, item)) # positional argument
+#print("The {} jumped over the {}".format(animal, item))
+print("The {1} jumped over the {0}".format(animal, item)) # positional argument
+#print("The {animal} jumped over the {item}".format(animal="cow", item="moon")) # keyword argument
