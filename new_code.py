757266cabb51b5c4687b2050d2e82c4eb4af348c
lazyb0y2 <mjones60@bostonk12.org>
2024-03-21 08:54:50 -0400

    new lines

--- new_code.py
+++ new_code.py
@@ -62,6 +62,8 @@
 #         A variable is only available from inside the region it is created.
 #         A global and locally scoped version of a variable can be created
 
+
+
 def display_name():
     name = "Code"   # local scope (available only inside this function)
     print(name)
