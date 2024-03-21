967c6207d1a804db271d1fa8895b77c9236802ab
lazyb0y2 <mjones60@bostonk12.org>
2024-03-21 10:34:48 -0400

    working on *args #3

--- new_code.py
+++ new_code.py
@@ -65,10 +65,13 @@
 # ----------------------------------------------------------------------------------------------------------------------
 # *args =   parameter that will pack all arguments into a tuple
 #           useful so that a function can accept a varying number of arguments
-
-def add(num1, num2):
-    sum = num1 + num2
+    # (num1 + num2)
+def add(*args):
+    sum = 0
+    for i in args:
+        sum += i
+    #sum = num1 + num2
     return sum
 
 
-print(add(1, 2))
+print(add(1, 2, 10, 400))
