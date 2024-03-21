7fc38ba56542e1cf3313e878ac69a62e229b21d0
lazyb0y2 <mjones60@bostonk12.org>
2024-03-21 13:55:00 -0400

    Working on str.format #8

--- new_code.py
+++ new_code.py
@@ -81,12 +81,18 @@
 # str.format() =    optional method that gives users
 #                   more control when displaying output
 
-#animal = "cow"
-#item = "moon"
+animal = "cow"
+item = "moon"
 
 #print("The "+animal+" jumped over the "+item)
 #print("The {} jumped over the {}".format(animal, item))
 #print("The {1} jumped over the {0}".format(animal, item)) # positional argument
 #print("The {animal} jumped over the {item}".format(animal="cow", item="moon")) # keyword argument
 
-text = "The {} jump over the {}"
+#text = "The {} jump over the {}"
+#print(text.format(animal, item))
+
+name2 = "dude"
+
+print("Hello my name is {}".format(name2))
+print("Hello my name is {:}".format(name2))
