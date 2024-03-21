2110132e06ec5fb0136d41c0e52133bbfae4e64b
lazyb0y2 <mjones60@bostonk12.org>
2024-03-21 08:36:43 -0400

    new line

--- new_code.py
+++ new_code.py
@@ -61,3 +61,4 @@ slice = slice(8, -4)
 # scope = The region that a variable is recognized
 #         A variable is only available from inside the region it is created.
 #         A global and locally scoped version of a variable can be created
+
