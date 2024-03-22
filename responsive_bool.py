46fd02e7613e45d3e3b38318c2781d84358e2aa3
lazyb0y2 <mjones60@bostonk12.org>
2024-03-22 10:40:07 -0400

    exception #1

--- responsive_bool.py
+++ responsive_bool.py
@@ -86,3 +86,7 @@
 # ----------------------------------------------------------------------------------------------------------------------
 # exception =   event detected during execution that interrupt the flow of a program
 
+numerator = int(input("Enter a number to divide: "))
+denominator = int(input("Enter a number to divide by: "))
+result = numerator / denominator
+print(result)
