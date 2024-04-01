0729c0779ddc9ab721cead963e8b4641590a4612
lazyb0y2 <mjones60@bostonk12.org>
2024-04-01 10:35:35 -0400

    file detection #6

--- new_code.py
+++ new_code.py
@@ -129,13 +129,15 @@ import random
 # ----------------------------------------------------------------------------------------------------------------------
 import os
 
-path = "/Users/319501/Downloads"
+#path = "/Users/319501/Downloads"
 
-if os.path.exists(path):
-    print("That location exists!")
-    if os.path.isfile(path):
-        print("That is a file")
-    elif os.path.isdir(path):
-        print("That's a directory")
-else:
-    print("That location doesn't exist!")
+#if os.path.exists(path):
+#    print("That location exists!")
+#    if os.path.isfile(path):
+#        print("That is a file")
+#    elif os.path.isdir(path):
+#        print("That's a directory")
+#else:
+#    print("That location doesn't exist!")
+
+
