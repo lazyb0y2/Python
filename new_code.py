6b84f66c1337c0abd18b975afbe752f514dc3a8a
lazyb0y2 <mjones60@bostonk12.org>
2024-04-01 09:42:32 -0400

    file detection #3

--- new_code.py
+++ new_code.py
@@ -133,6 +133,7 @@ path = "/Users/319501/Downloads"
 
 if os.path.exists(path):
     print("That location exists!")
-
+    if os.path.isfile(path):
+        print("That is a file")
 else:
     print("That location doesn't exist!")
