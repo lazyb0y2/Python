f1860a709ca9f57c90d3be0f78991846ec381186
lazyb0y2 <mjones60@bostonk12.org>
2024-04-01 13:45:20 -0400

    modules #6

--- new_code.py
+++ new_code.py
@@ -142,7 +142,10 @@ import os
 # ----------------------------------------------------------------------------------------------------------------------
 # module = a file containing python code, May contain functions, classes, ect.
 # Used with modular programming, which is to separate a program into parts
-import messages
 
-messages.hello()
-messages.bye()
+#import message
+#import message as msg
+from messages import hello, bye
+
+hello()
+bye()
